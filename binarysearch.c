5187fedcc7ab1e894ac4253a724d0411ff442901
MD ISMAIL HOSEN <43248620+ismail61@users.noreply.github.com>
2018-09-25 22:42:50 +0600

    Update binarysearch.c

--- binarysearch.c
+++ binarysearch.c
@@ -1,43 +1,41 @@
-#include<stdio.h>//binary search r  time complexity=o(log n)
-                   //binary search r space complexity=o(1)
+#include<stdio.h>
 
 int main()
 {
     int low_index,high_index,mid,i;
-    int a[10];//n=10;
+    int a[10];
     for(i=0;i<10;i++)
     {
-        scanf("%d",&a[i]);//scan 10 element of array...
+        scanf("%d",&a[i]);
     }
     low_index=0;
-    high_index=9;//high_index=n-1;
+    high_index=9;
     int num;
     printf("enter the number which searching");
-    scanf("%d",&num);//searching a number;
-    while(low_index<=high_index)//low index jodi high index thake choto or soman hole loop cholbe
+    scanf("%d",&num);
+    while(low_index<=high_index)
     {
-        mid=(high_index+low_index)/2;//binary search low;
+        mid=(high_index+low_index)/2;
         if(num==a[mid])
         {
             break;
         }
         if(num<a[mid])
         {
-            high_index=mid-1;//numberti mid index thake choto hole high index 1 kom hobe;
+            high_index=mid-1;
         }
         if(num>a[mid])
         {
-            low_index=mid+1;//numberti mid index thake boro hole low index 1 besi hobe..
+            low_index=mid+1;
         }
 
     }
-    if(low_index>high_index)//jodi low index high index r cheya boro hoy tahole search hobe na....
+    if(low_index>high_index)
     {
         printf("%d is not found",num);
     }
     else
         printf("%d is found\n",a[mid]);
-        printf("%d is found\n",num);
         printf("%d th element of array",mid);
 
     return 0;
