74fc07d493e550b64850d30e066ffa962fb0fbb7
MD ISMAIL HOSEN <43248620+ismail61@users.noreply.github.com>
2019-01-05 22:38:59 +0600

    Add files via upload

---
+++ patternmatch.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std ;
+void match(char *str , char *pat ) {
+    int ls = strlen(str) ;
+    int lp = strlen(pat) ;
+    int i , j , c = 0 ;
+    for( i = 0 ; i < ls - lp ; i ++ ) {
+        for( j = 0 ; j < lp ; j ++ ) {
+            if( str[i+j] != pat[j]) break ;
+        }
+        if( j == lp ) {
+                cout<<"Pattern found at "<<i+1<<" location"<<endl;
+                c ++ ;
+        }
+    }
+    if( c == 0 ) cout<<"Pattern not found\n"<<endl;
+}
+int main() {
+
+    char *str = "what not to do" ;
+    char *pat = "no" ;
+    match(str , pat) ;
+return 0 ;
+}
+
